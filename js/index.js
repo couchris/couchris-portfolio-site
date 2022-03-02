1a947f04b9a7b3a73c91f38185f7e6baffa4d9cb
Chris Couto <couchris@sheridancollege.ca>
2022-03-02 16:24:37 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,98 @@
+window.addEventListener("DOMContentLoaded", (e) => {
+  var scrollLinks = document.getElementsByClassName("scroll");
+
+  for (let link of scrollLinks) {
+    link.addEventListener("click", (e) => {
+      document
+        .querySelector(e.target.id)
+        .scrollIntoView({ behavior: "smooth" });
+    });
+  }
+});
+
+//take all the <anchor tags> that have the class scroll
+//take its id, add a dot infront of it to search for that name as a css class on another element on the page
+//use scrollIntoView function
+//make sure section's class matches the id of the link
+
+$(document).ready(function(){
+  console.log($('nav'));
+  $(".container").on("scroll", function () {
+  var distance = 500;
+        if ($(".container").scrollTop() > distance) {
+            $('nav').addClass('navscroll');
+            console.log("Hello");
+        }
+        else {
+          $('nav').removeClass('navscroll');
+            console.log("Bye");
+        }
+        
+   });
+});
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+  this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+  this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+  delta = this.period;
+  this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+  this.isDeleting = false;
+  this.loopNum++;
+  delta = 500;
+  }
+
+  setTimeout(function() {
+  that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-type');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
+      }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
+};
